7ea0bb90cf04b51f37052dafd9e9586e20e82c10
Roman <x6wert@gmail.com>
2020-01-20 16:52:31 +0300

    +++

---
+++ Assets/Scripts/Controllers/Raycaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+        public Transform target;
+    void Update()
+    {
+        //сюда запишется инфо о пересечении луча, если оно будет
+        RaycastHit hit;
+        //сам луч, начинается от позиции этого объекта и направлен в сторону цели
+        Ray ray = new Ray(transform.position, transform.forward);
+        //пускаем луч
+        Physics.Raycast(ray, out hit);
+
+        //если луч с чем-то пересёкся, то..
+        if (hit.collider != null)
+        {
+            //если луч не попал в цель
+            if (hit.collider.gameObject != target.gameObject)
+            {
+                Debug.Log("Путь к врагу преграждает объект: " + hit.collider.name);
+            }
+            //если луч попал в цель
+            else
+            {
+                Debug.Log("Попадаю во врага!!!");
+            }
+            //просто для наглядности рисуем луч в окне Scene
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+        }
+    }
+}
